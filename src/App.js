ddb4360ca87be6b378a9c7403131c63f7b7b8412
brandiw <brandimichelle.0911@gmail.com>
2019-03-06 10:19:04 -0800

    Adding in-class code-along

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      num1: '',
+      num2: '',
+      answer: '',
+      error: ''
+    };
+  }
+
+  setNum = (e, num) => {
+    this.setState({ [num]: e.target.value});
+  }
+
+  add = () => {
+    if(this.state.num1 && this.state.num2){
+      let answer = Number(this.state.num1) + Number(this.state.num2);
+      this.setState({ answer: answer, num1: '', num2: '', error: ''});
+    }
+    else {
+      this.setState({ answer: '', num1: '', num2: '', error: 'You messed up somehow...'});
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          OH LOOK AT MY CALCULATOR!
         </header>
+        <div>
+          <h3 className="red">{this.state.error}</h3>
+          <h2>Num 1</h2>
+          <input type="number"
+             name="num1"
+             placeholder="Enter your first number"
+             value={this.state.num1}
+             onChange={ (e) => this.setNum(e, 'num1') }
+          />
+          <h2>PLUS Num 2</h2>
+          <input type="number"
+             name="num2"
+             placeholder="Enter your second number"
+             value={this.state.num2}
+             onChange={ (e) => this.setNum(e, 'num2') }
+          />
+          <br />
+          <button onClick={this.add}>EQUALS!!!</button>
+          <h2>==================</h2>
+          <h3>{this.state.answer}</h3>
+        </div>
       </div>
     );
   }
